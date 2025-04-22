274cb56fb292386512c4d0cce2f50b6556981b33
EunHyeok <secun77@naver.com>
2025-04-22 09:37:30 +0900

    초기커밋

---
+++ src/test/java/com/playdata/orderserviceback/OrderserviceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.orderserviceback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderserviceBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
