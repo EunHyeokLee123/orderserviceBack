cdab5a34239557896c1d0cd1747f98110277ab67
EunHyeok <secun77@naver.com>
2025-04-22 11:08:52 +0900

    User insert 테스트

---
+++ src/main/java/com/playdata/orderserviceback/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.playdata.orderserviceback.user.repository;
+
+import com.playdata.orderserviceback.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
